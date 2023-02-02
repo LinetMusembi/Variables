2bb265467afd9fde15e27e9a196748af5fa16f3d
linetmusembi <musembilinetmwongeli@gmail.com>
2023-02-02 21:29:37 +0300

    new file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name ="Linet-Musembi"
+    println(name)
+    var age :Byte = 18
+    println(age)
+    var phonenumber :String = "0798032314"
+    println(phonenumber)
+    val weight =50
+    println(weight)
+    var citizenship ="Kenyan"
+    println(citizenship)
+
+}
